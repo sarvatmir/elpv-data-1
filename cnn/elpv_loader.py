91c8875d14b3bf4276e42ce5adbcae61fdbc9e3b
Sarwat Ali <68585738+sarvatmir@users.noreply.github.com>
2026-02-25 14:24:56 +0530

    Update elpv_loader.py

--- cnn/elpv_loader.py
+++ cnn/elpv_loader.py
@@ -16,7 +16,7 @@ images, proba, types = load_dataset()
 # Transforms for DARTS (32×32 RGB)
 # -------------------------------
 transform = T.Compose([
-    T.Resize((32, 32)),
+    T.Resize((224, 224)),
     T.ToTensor(),  # PIL → Tensor
 ])
 
