830144907a38adcb4b77aadc3de9256a181c92c4
Sarwat Ali <68585738+sarvatmir@users.noreply.github.com>
2026-02-25 15:10:17 +0530

    Update train.py

--- cnn/train.py
+++ cnn/train.py
@@ -83,16 +83,6 @@ def main():
       weight_decay=args.weight_decay
       )
 
-"""train_transform, valid_transform = utils._data_transforms_cifar10(args)
-  train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
-  valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
-
-  train_queue = torch.utils.data.DataLoader(
-      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
-
-  valid_queue = torch.utils.data.DataLoader(
-      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)"""
-
   # new code for elpv
   train_queue = train_loader
   valid_queue = test_loader  
