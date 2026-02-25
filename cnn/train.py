4afe041510a5c3ec32ad28f25dae6aa5291d870f
Sarwat Ali <68585738+sarvatmir@users.noreply.github.com>
2026-02-25 14:30:22 +0530

    Update train.py

--- cnn/train.py
+++ cnn/train.py
@@ -28,7 +28,7 @@ parser.add_argument('--report_freq', type=float, default=50, help='report freque
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--epochs', type=int, default=40, help='num of training epochs')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
-parser.add_argument('--layers', type=int, default=20, help='total number of layers')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers') #20 in darts
 parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
 parser.add_argument('--auxiliary_weight', type=float, default=0.4, help='weight for auxiliary loss')
