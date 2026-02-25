f5708a8441f0aa4560c91f32b28e41433f01367e
Sarwat Ali <68585738+sarvatmir@users.noreply.github.com>
2026-02-25 12:41:18 +0530

    Create elpv_loader.py
    Add your dataset code inside elpv_loader.py

---
+++ cnn/elpv_loader.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision.transforms as T
+from torch.utils.data import Dataset, DataLoader
+from elpv_dataset.utils import load_dataset
+import numpy as np
+from PIL import Image
+
+# Load dataset
+images, proba, types = load_dataset()
+
+transform = T.Compose([
+    T.Resize((32, 32)),
+    T.Grayscale(num_output_channels=3),
+    T.ToTensor(),
+])
+
+class ELPVDataset(Dataset):
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        img = self.images[idx]
+        img = Image.fromarray(img)
+        label = int(self.labels[idx])
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+# Make the dataset
+dataset = ELPVDataset(images, types, transform)
+
+# Split
+train_size = int(0.8 * len(dataset))
+test_size  = len(dataset) - train_size
+train_set, test_set = torch.utils.data.random_split(dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_set, batch_size=64, shuffle=True)
+test_loader  = DataLoader(test_set, batch_size=64)
